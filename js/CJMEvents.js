7ccd627a42440f6cf285ac32ef4df8f046eacbbc
Clint Milner <clint@sympoz.com>
2015-05-21 22:52:36 +0100

    Cleaned up, ready for hosting

--- js/CJMEvents.js
+++ js/CJMEvents.js
@@ -14,7 +14,6 @@ var CJMEvents = ( function()
 
             $html.removeClass( 'no-js' ).addClass( 'js' );
 
-            // go get the data
             cmFitTrack.on( 'value', function(snapshot) {
                 console.log(snapshot.val());
 
@@ -63,7 +62,6 @@ var CJMEvents = ( function()
         randomRGBA: function()
         {
             return 'rgba(' + CJMEvents.getRandom( 0, 255 ) + ',' + CJMEvents.getRandom( 0, 255 ) + ',' + CJMEvents.getRandom( 0, 255 ) + ',' +  CJMEvents.getRandom( .3, 1 ) + ')';
-
         },
         getRandom: function( min, max )
         {
@@ -88,81 +86,7 @@ var CJMEvents = ( function()
             {
                 $( 'nav' ).removeClass( 'navbar-inverse' )
                     .addClass( 'bg-danger' );
-
             }
-
-        },
-        getStats: function( snapshot )
-        {
-            var bodyWeight = [],
-                boneWeight = [],
-                waterPct = [],
-                fatPct = [],
-                date = [];
-
-            cmFitTrack.on( 'value', function(snapshot) {
-            var stats = snapshot.val();
-
-                for (var key in stats) {
-                    if (stats.hasOwnProperty(key)) {
-                        bodyWeight.push(stats[key].bodyWeight);
-                        boneWeight.push(stats[key].boneWeight);
-                        waterPct.push(stats[key].waterPct);
-                        fatPct.push(stats[key].fatPct);
-                        date.push(stats[key].date);
-                    }
-                }
-
-            }, function (errorObject) {
-                console.log("The read failed: " + errorObject.code);
-            });
-
-            return {
-                labels : date,
-                datasets : [
-                    {
-                        label: "Body Weight",
-                        fillColor : "rgba(220,220,220,0.2)",
-                        strokeColor : "rgba(220,220,220,1)",
-                        pointColor : "rgba(220,220,220,1)",
-                        pointStrokeColor : "#fff",
-                        pointHighlightFill : "#fff",
-                        pointHighlightStroke : "rgba(220,220,220,1)",
-                        data : bodyWeight
-                    },
-                    {
-                        label: "Bone Weight",
-                        fillColor : "rgba(151,187,205,0.2)",
-                        strokeColor : "rgba(151,187,205,1)",
-                        pointColor : "rgba(151,187,205,1)",
-                        pointStrokeColor : "#fff",
-                        pointHighlightFill : "#fff",
-                        pointHighlightStroke : "rgba(151,187,205,1)",
-                        data : boneWeight
-                    },
-                    {
-                        label: "Fat Percentage",
-                        fillColor : "rgba(220,220,220,0.2)",
-                        strokeColor : "rgba(220,220,220,1)",
-                        pointColor : "rgba(220,220,220,1)",
-                        pointStrokeColor : "#fff",
-                        pointHighlightFill : "#fff",
-                        pointHighlightStroke : "rgba(220,220,220,1)",
-                        data : fatPct
-                    },
-                    {
-                        label: "Water Percentage",
-                        fillColor : "rgba(151,187,205,0.2)",
-                        strokeColor : "rgba(151,187,205,1)",
-                        pointColor : "rgba(151,187,205,1)",
-                        pointStrokeColor : "#fff",
-                        pointHighlightFill : "#fff",
-                        pointHighlightStroke : "rgba(151,187,205,1)",
-                        data : waterPct
-                    }
-                ]
-
-            };
         },
         drawChart: function( data, canvasId )
         {
